92f3a86d17c3272d384d389933855d72e50471b6
Gbenga Cruz <gbenga.cruz@gmail.com>
2025-10-17 21:49:49 +0100

    Add files via upload

---
+++ sql/ollama_audit_log.sql
@@ -0,0 +1,56 @@
+--------------------------------------------------------
+--  File created - Friday-October-17-2025   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table OLLAMA_AUDIT_LOG
+--------------------------------------------------------
+
+  CREATE TABLE "HRMS"."OLLAMA_AUDIT_LOG" 
+   (	"AUDIT_ID" NUMBER, 
+	"CREATED_AT" TIMESTAMP (6) WITH TIME ZONE DEFAULT SYSTIMESTAMP, 
+	"REQUESTOR" VARCHAR2(200 BYTE) COLLATE "USING_NLS_COMP", 
+	"TABLE_NAME" VARCHAR2(128 BYTE) COLLATE "USING_NLS_COMP", 
+	"MODEL" VARCHAR2(200 BYTE) COLLATE "USING_NLS_COMP", 
+	"USER_PROMPT" CLOB COLLATE "USING_NLS_COMP", 
+	"REQUEST_BODY" CLOB COLLATE "USING_NLS_COMP", 
+	"RESPONSE_JSON" CLOB COLLATE "USING_NLS_COMP", 
+	"GENERATED_SQL" CLOB COLLATE "USING_NLS_COMP", 
+	"EXECUTION_RESULT" CLOB COLLATE "USING_NLS_COMP", 
+	"STATUS" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP", 
+	"ERROR_MESSAGE" CLOB COLLATE "USING_NLS_COMP"
+   ) ;
+--------------------------------------------------------
+--  DDL for Index SYS_C00102114
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "SYS_C00102114" ON "OLLAMA_AUDIT_LOG" ("AUDIT_ID");
+--------------------------------------------------------
+--  DDL for Trigger OLLAMA_AUDIT_LOG_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE  TRIGGER "OLLAMA_AUDIT_LOG_TRG" BEFORE INSERT OR UPDATE ON "OLLAMA_AUDIT_LOG" 
+   FOR EACH ROW 
+ BEGIN 
+
+	if :new.audit_id is null then
+	   :new.audit_id := ollama_audit_seq.nextval;
+    end if;
+
+    IF inserting then
+        :new.CREATED_AT := localtimestamp;
+        :new.REQUESTOR := (nvl(sys_context('APEX$SESSION','APP_USER'),user));
+    END IF;
+
+ END OLLAMA_AUDIT_LOG_TRG;
+/
+ALTER TRIGGER "OLLAMA_AUDIT_LOG_TRG" ENABLE;
+--------------------------------------------------------
+--  Constraints for Table OLLAMA_AUDIT_LOG
+--------------------------------------------------------
+
+  ALTER TABLE "OLLAMA_AUDIT_LOG" ADD PRIMARY KEY ("AUDIT_ID") ENABLE;
+  
+/
+
+CREATE SEQUENCE  "OLLAMA_AUDIT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 43 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+/
