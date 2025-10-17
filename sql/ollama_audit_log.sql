2aa5045768af825aef355ea0c2dacd1a12767b33
Cruz Bello <gbenga.cruz@gmail.com>
2025-10-18 00:52:09 +0100

    Refactor OLLAMA_AUDIT_LOG DDL and trigger

--- sql/ollama_audit_log.sql
+++ sql/ollama_audit_log.sql
@@ -5,47 +5,45 @@
 --  DDL for Table OLLAMA_AUDIT_LOG
 --------------------------------------------------------
 
-  CREATE TABLE "OLLAMA_AUDIT_LOG" 
-   (	"AUDIT_ID" NUMBER, 
-	"CREATED_AT" TIMESTAMP (6) WITH TIME ZONE DEFAULT SYSTIMESTAMP, 
-	"REQUESTOR" VARCHAR2(200 BYTE) COLLATE "USING_NLS_COMP", 
-	"TABLE_NAME" VARCHAR2(128 BYTE) COLLATE "USING_NLS_COMP", 
-	"MODEL" VARCHAR2(200 BYTE) COLLATE "USING_NLS_COMP", 
-	"USER_PROMPT" CLOB COLLATE "USING_NLS_COMP", 
-	"REQUEST_BODY" CLOB COLLATE "USING_NLS_COMP", 
-	"RESPONSE_JSON" CLOB COLLATE "USING_NLS_COMP", 
-	"GENERATED_SQL" CLOB COLLATE "USING_NLS_COMP", 
-	"EXECUTION_RESULT" CLOB COLLATE "USING_NLS_COMP", 
-	"STATUS" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP", 
-	"ERROR_MESSAGE" CLOB COLLATE "USING_NLS_COMP"
-   ) ;
+create table "OLLAMA_AUDIT_LOG" (
+   "AUDIT_ID"         number primary key,
+   "CREATED_AT"       timestamp(6) with time zone default systimestamp,
+   "REQUESTOR"        varchar2(200 byte) collate "USING_NLS_COMP",
+   "TABLE_NAME"       varchar2(128 byte) collate "USING_NLS_COMP",
+   "MODEL"            varchar2(200 byte) collate "USING_NLS_COMP",
+   "USER_PROMPT"      clob collate "USING_NLS_COMP",
+   "REQUEST_BODY"     clob collate "USING_NLS_COMP",
+   "RESPONSE_JSON"    clob collate "USING_NLS_COMP",
+   "GENERATED_SQL"    clob collate "USING_NLS_COMP",
+   "EXECUTION_RESULT" clob collate "USING_NLS_COMP",
+   "STATUS"           varchar2(50 byte) collate "USING_NLS_COMP",
+   "ERROR_MESSAGE"    clob collate "USING_NLS_COMP"
+);
+
 --------------------------------------------------------
 --  DDL for Trigger OLLAMA_AUDIT_LOG_TRG
 --------------------------------------------------------
+create sequence "OLLAMA_AUDIT_SEQ" minvalue 1 maxvalue 9999999999999999999999999999 increment by 1 start with 1;
 
-  CREATE OR REPLACE  TRIGGER "OLLAMA_AUDIT_LOG_TRG" BEFORE INSERT OR UPDATE ON "OLLAMA_AUDIT_LOG" 
-   FOR EACH ROW 
- BEGIN 
 
-	if :new.audit_id is null then
-	   :new.audit_id := ollama_audit_seq.nextval;
-    end if;
+create or replace trigger "OLLAMA_AUDIT_LOG_TRG" before
+   insert or update on "OLLAMA_AUDIT_LOG"
+   for each row
+begin
+   if :new.audit_id is null then
+      :new.audit_id := ollama_audit_seq.nextval;
+   end if;
 
-    IF inserting then
-        :new.CREATED_AT := localtimestamp;
-        :new.REQUESTOR := (nvl(sys_context('APEX$SESSION','APP_USER'),user));
-    END IF;
+   if inserting then
+      :new.created_at := localtimestamp;
+      :new.requestor := ( nvl(
+         sys_context(
+            'APEX$SESSION',
+            'APP_USER'
+         ),
+         user
+      ) );
+   end if;
 
- END OLLAMA_AUDIT_LOG_TRG;
-/
-ALTER TRIGGER "OLLAMA_AUDIT_LOG_TRG" ENABLE;
---------------------------------------------------------
---  Constraints for Table OLLAMA_AUDIT_LOG
---------------------------------------------------------
-
-  ALTER TABLE "OLLAMA_AUDIT_LOG" ADD PRIMARY KEY ("AUDIT_ID") ENABLE;
-  
-/
-
-CREATE SEQUENCE  "OLLAMA_AUDIT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 43 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+end ollama_audit_log_trg;
 /
